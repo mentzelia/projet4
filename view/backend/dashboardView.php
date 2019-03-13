f8c7009f2132843a805cdbec55ae0050925a7b78
mentzelia <mentzelia@hotmail.fr>
2019-03-13 08:24:32 -1000

    ajout des liens pour les actions sur tableau de bord

--- view/backend/dashboardView.php
+++ view/backend/dashboardView.php
@@ -3,6 +3,16 @@
 <?php ob_start(); ?>
 
 <h1>Bienvenue sur votre tableau de bord!</h1>
+
+<p>
+<a href="">Ecrire un billet</a><br/>
+<a href="">Modifier un billet</a><br/>
+<a href="">Effacer un billet</a><br/>
+<a href="">Modérer un commentaire</a><br/>
+</p>
+
+<p><a href="">Se déconnecter</a>
+</p>
         
         
 
