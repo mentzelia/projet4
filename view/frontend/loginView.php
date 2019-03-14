8e11bef915a1dd34e637925bf694444b91001f67
mentzelia <mentzelia@hotmail.fr>
2019-03-14 09:41:33 -1000

    ajour lien retour page d'accueil sur les vues inscription et connexion

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -4,16 +4,19 @@
 
 <h1>Connexion à l'administration</h1>
         
-        <form method="post" action="index.php?action=admin_connexion">
-            <label for="login">Nom d'utilisateur:</label>
-            <input type="text" name="login" value="" id="login" /><br />
+<form method="post" action="index.php?action=admin_connexion">
+    <label for="login">Nom d'utilisateur:</label>
+    <input type="text" name="login" value="" id="login" /><br />
 
-            <label for="password">Mot de passe:</label>
-            <input type="password" name="password" value="" id="password" /><br />
+    <label for="password">Mot de passe:</label>
+    <input type="password" name="password" value="" id="password" /><br />
 
-            <input type="submit" value="Se connecter" />
-        </form>
-        
+    <input type="submit" value="Se connecter" />
+</form>
+
+<p>
+    <a href="index.php">Retour à la page d'accueil</a>
+</p>        
 
 
 <?php $content = ob_get_clean(); ?>
--- view/frontend/registerView.php
+++ view/frontend/registerView.php
@@ -4,21 +4,25 @@
 
 <h1>Inscription</h1>
         
-        <form method="post" action="index.php?action= user_registration">
-            <label for="login">Nom d'utilisateur:</label>
-            <input type="text" name="login" value="" id="login" /><br />
+<form method="post" action="index.php?action= user_registration">
+    <label for="login">Nom d'utilisateur:</label>
+    <input type="text" name="login" value="" id="login" /><br />
 
-            <label for="password1">Mot de passe:</label>
-            <input type="password" name="password1" value="" id="password1" /><br />
+    <label for="password1">Mot de passe:</label>
+    <input type="password" name="password1" value="" id="password1" /><br />
 
-            <label for="password2">Retapez le mot de passe:</label>
-            <input type="password" name="password2" value="" id="password2" /><br />
+    <label for="password2">Retapez le mot de passe:</label>
+    <input type="password" name="password2" value="" id="password2" /><br />
 
-            <label for="email">Adresse email:</label>
-            <input type="email" name="email" value="" id="email" /><br />
+    <label for="email">Adresse email:</label>
+    <input type="email" name="email" value="" id="email" /><br />
 
-            <input type="submit" value="S'inscrire" />
-        </form>
+    <input type="submit" value="S'inscrire" />
+</form>
+
+<p>
+    <a href="index.php">Retour à la page d'accueil</a>
+</p>
         
 
 
