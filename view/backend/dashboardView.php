daa78cd09213e32fdb546832640a0d3d53b4d730
mentzelia <mentzelia@hotmail.fr>
2019-03-28 19:00:17 +0100

    css dashboard réglé. Reste à importer les commentaires en php

--- view/backend/dashboardView.php
+++ view/backend/dashboardView.php
@@ -2,15 +2,23 @@
 
 <?php ob_start(); ?>
 
-<h1>Bienvenue sur votre tableau de bord!</h1><br />
+<h2>Bienvenue sur votre tableau de bord!</h2><br />
 <h3>Que voulez-vous faire?</h3>
 
-<p>
-<a href="index.php?action=createNewPost">Ecrire un billet</a><br/>
-<a href="index.php?action=getPostToModify">Modifier ou supprimer un billet</a><br/>
-<a href="index.php?action=manageComments">Modérer un commentaire</a><br/>
-<a href="index.php?action=log_out">Se déconnecter</a>
-</p>
+<div class="dashboard">
+    <div class="actions">
+        <p>
+            <a href="index.php?action=createNewPost">Ecrire un billet</a><br/>
+            <a href="index.php?action=getPostToModify">Modifier ou supprimer un billet</a><br/>
+            <a href="index.php?action=manageComments">Modérer un commentaire</a><br/>
+            <a href="index.php?action=log_out">Se déconnecter</a>
+        </p>
+    </div>
+    
+    <div class="lastNews">
+        <p>TEST</p>
+    </div>
+</div>
      
 
 
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,9 +3,8 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="public/css/style.css" rel="stylesheet" />
         <link href="public/css/normalize.css" />
-        
+        <link href="public/css/frontend.css" rel="stylesheet" />        
         <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Montserrat|Reenie+Beanie|Special+Elite" rel="stylesheet">
         
          
